0d32e854e0f456bb70b80bb60a454cc259b1b7b5
Lena <len.dal@yandex.ru>
2022-06-07 23:15:11 +0300

    third task

---
+++ N3/Program.cs
@@ -0,0 +1,10 @@
+﻿int a;
+Console.WriteLine("Введи любое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+{
+    if (a % 2 == 0)
+    Console.WriteLine($"Число {a} четное");
+    else 
+    Console.WriteLine($"Число {a} нечетное");
+}
