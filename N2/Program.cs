0053b0b1025163d8d6d3d7e5ee0f8e9c19ea9e68
Lena <len.dal@yandex.ru>
2022-06-07 22:58:42 +0300

    second task

---
+++ N2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b, c;
+Console.WriteLine("Введи первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b) 
+{
+    if(a > c)
+    Console.WriteLine($"Число {a} максимальное");
+}
+if(b > a) 
+{
+    if(b > c)
+    Console.WriteLine($"Число {b} максимальное");
+}
+if(c > a) 
+{
+    if(c > b)
+    Console.WriteLine($"Число {c} максимальное");
+}
